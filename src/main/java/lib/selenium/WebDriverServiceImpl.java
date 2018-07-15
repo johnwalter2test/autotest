0f5007ad412ea7d17438be666e615a9a6e0a392a
John <John@DESKTOP-G1T8O2E>
2018-07-15 10:30:30 +0530

    test merge

--- src/main/java/lib/selenium/WebDriverServiceImpl.java
+++ src/main/java/lib/selenium/WebDriverServiceImpl.java
@@ -154,5 +154,9 @@ public class WebDriverServiceImpl extends WebDriverListener implements WebDriver
 		driver.getCurrentUrl();
 		
 	}
+	public void verifyurl1() {
+		driver.getCurrentUrl();
+		
+	}
 
 }
